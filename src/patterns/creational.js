74a90964dbca1a61122dead1de3a1afc7abeebbd
AndreVeiga <soutomachado1@gmail.com>
2020-11-13 23:52:06 -0300

    Mais testes unitários

---
+++ src/patterns/creational.js
@@ -0,0 +1,8 @@
+/**
+ * Pattern mais simples em javascript.
+ * 
+ * @author Elton Veiga
+ */
+const createObj = () =>  new Object()
+
+module.exports = createObj
--- test/continuationPassing/file_test_1.spec.js
+++ test/continuationPassing/file_test_1.spec.js
@@ -2,11 +2,13 @@
  * Classe de testes do padrão Continuation Passing
  * @author Elton Veiga
  */
-const assert = require('assert');
-const pattern = require('../../src/patterns/continuation-passing');
+const assert = require('assert')
+const pattern = require('../../src/patterns/continuation-passing')
 
-describe('teste para soma', function() {
-  it('Testando função soma', function() {
-    assert.equal(pattern.soma(2, 3), 5)
-  });
-});
+describe('Teste para o Continuation Passing', function() {
+  it('Testando função soma syncrona', () => assert.equal(pattern.soma(2, 3), 5))
+
+  it('Testando função soma assyncrona', () => {
+      
+  })
+})
