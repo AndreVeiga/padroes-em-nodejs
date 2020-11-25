a9782af7e4f0422e896acb78c8767b5cb987a3e9
Andre Machado <soutomachado1@gmail.com>
2020-11-24 23:23:10 -0300

    add gitub flow config

--- src/patterns/singleton.js
+++ src/patterns/singleton.js
@@ -4,19 +4,20 @@
  * @author Elton Veiga
  */
 
-const Singleton = (function() {
-    function Singleton(args) {
-      const _args = args || {}
+const Singleton = (function () {
+    let _args
+    function Singleton (args) {
+        _args = args || {}
     }
 
     let instance
-    let _static = {
-      getInstance: function (args) {
-         if (instance === undefined) {
-           instance = new Singleton(args)
-         }
-         return Object.freeze(instance)
-      }
+    const _static = {
+        getInstance: function (args) {
+            if (instance === undefined) {
+                instance = new Singleton(args)
+            }
+            return Object.freeze(instance)
+        }
     }
     return Object.freeze(_static)
 })()
